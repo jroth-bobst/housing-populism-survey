1a429a0173fadef4c50ccb1b681cee02d3e109ea
jroth-bobst <jeremy.roth@princeton.edu>
2025-05-06 11:37:08 -0700

    updated zip code row size

--- Qualtrics/R_Code/gen_zip_code_test.R
+++ Qualtrics/R_Code/gen_zip_code_test.R
@@ -2,10 +2,9 @@ library(tidyverse)
 library(truncnorm)
 
 # generate placeholder dataset, where each row represents a zip code and a column shows the estimated share of instituion-owned homes in that zip code
-n_zip <- 30000
 set.seed(123)
-zip_codes <- sort(sample(10000:99999, size=n_zip))
-institutional_share <- round(rtruncnorm(n_zip, a=0, b=100, mean=15, sd=5), 
+zip_codes <- 10000:99999
+institutional_share <- round(rtruncnorm(length(zip_codes), a=0, b=100, mean=15, sd=5), 
                              1)
 
 df <- data.frame(zip_code=zip_codes,
